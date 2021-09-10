bf84304cad951b89579efb7affc394d81a2fcbfb
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-09-11 03:02:03 +1000

    Add command base

---
+++ src/commands/base.ts
@@ -0,0 +1,47 @@
+import {
+  ApplicationCommandData,
+  Client,
+  CommandInteraction,
+  Guild,
+  GuildMember,
+  MessagePayload,
+  WebhookEditMessageOptions,
+} from "discord.js";
+import { dotsimus } from "../index.js";
+
+export type CommandInfo = ApplicationCommandData & {
+  guildOnly?: true;
+  defer?: true;
+  ownerOnly?: true;
+};
+
+export type GuildCommandInteraction = CommandInteraction & {
+  guildId: string;
+  member: GuildMember;
+  guild: Guild;
+  inGuild(): true;
+};
+
+export type CommandResponse =
+  | string
+  | MessagePayload
+  | WebhookEditMessageOptions
+  | null;
+
+export class Command {
+  info: CommandInfo;
+  client: Client;
+  constructor(info: CommandInfo) {
+    this.info = info;
+    this.client = dotsimus;
+  }
+
+  execute(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    interaction: CommandInteraction
+  ): CommandResponse | Promise<CommandResponse> {
+    throw new Error(
+      `Execute function on default for ${this.info.name} command.`
+    );
+  }
+}
