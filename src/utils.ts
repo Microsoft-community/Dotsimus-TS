65c7d42795d47d86078b434c9a55499cbfe1b152
08-k <cldoc507@outlook.com>
2021-09-11 19:51:51 -0400

    Create utils.ts
    Create utils.ts and a function used in index.ts. I don't feel like describing it further, you have eyes

---
+++ src/utils.ts
@@ -0,0 +1,6 @@
+export function FormatString(str: string, ... val: string[]):string {
+  for (let index = 0; index < val.length; index++) {
+    str = str.replace(`{${index}}`, val[index]);
+  }
+  return str;
+}
